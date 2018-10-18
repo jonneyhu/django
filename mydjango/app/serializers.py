523938ba11b299ef6e455cc892582f44e8597448
jonney <jonney.hu@blockheaders.com>
2018-10-18 18:07:35 +0800

    add

--- mydjango/app/serializers.py
+++ mydjango/app/serializers.py
@@ -1,14 +1,57 @@
 from django.contrib.auth.models import User,Group
 from rest_framework import serializers
+from app.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model=User
-        fields=('url','username','email','groups')
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Group
         fields=('url','name')
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    # groups=GroupSerializer()
+    class Meta:
+        model=User
+        fields=('url','username','email','groups')
+
+    # def create(self, validated_data):
+    #     groups=validated_data.pop('groups')
+    #     user=User.objects.create(**validated_data)
+    #     Group.objects.create(user=user,**groups)
+    #     return user
+    #
+    # def update(self, instance, validated_data):
+    #     group_data=validated_data.pop('groups')
+    #     instance.update(**validated_data)
+    #     instance.groups.update(**group_data)
+    #     return instance
+
+
+class SnippetSerializer(serializers.Serializer):
+    # id = serializers.IntegerField(read_only=True)
+    # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    # code = serializers.CharField(style={'base_template': 'textarea.html'})
+    # linenos = serializers.BooleanField(required=False)
+    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+    #
+    # def create(self, validated_data):
+    #     """
+    #     根据提供的验证过的数据创建并返回一个新的`Snippet`实例。
+    #     """
+    #     return Snippet.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     """
+    #     根据提供的验证过的数据更新和返回一个已经存在的`Snippet`实例。
+    #     """
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.code = validated_data.get('code', instance.code)
+    #     instance.linenos = validated_data.get('linenos', instance.linenos)
+    #     instance.language = validated_data.get('language', instance.language)
+    #     instance.style = validated_data.get('style', instance.style)
+    #     instance.save()
+    #     return instance
+    class Meta:
+        model=Snippet
+        fields=('id', 'title', 'code', 'linenos', 'language', 'style')
--- mydjango/app/views.py
+++ mydjango/app/views.py
@@ -1,9 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from django.contrib.auth.models import User,Group
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
-from  app.serializers import UserSerializer,GroupSerializer
+from rest_framework.parsers import JSONParser
+
+from app.models import Snippet
+from app.serializers import UserSerializer, GroupSerializer, SnippetSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -14,4 +19,41 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
       queryset = Group.objects.all()
       serializer_class = GroupSerializer
+
+class JSONResponse(HttpResponse):
+      def __init__(self ,data,**kwargs):
+          from rest_framework.renderers import JSONRenderer
+          content=JSONRenderer().render(data)
+          kwargs['content_type']='application/json'
+          super(JSONResponse,self).__init__(content,**kwargs)
+@csrf_exempt
+def snippet_list(request):
+    if request.method=="GET":
+        snippets=Snippet.objects.all()
+        serializer=SnippetSerializer(snippets,many=True)
+        data=serializer.data
+        return JSONResponse(data)
+    elif request.method=="POST":
+        data=JSONParser().parse(request)
+        serializer=SnippetSerializer(data=data)
+        if serializer.is_valid():
+             serializer.save()
+             return JSONResponse(data,status=200)
+        return JSONResponse(serializer.errors,status=400)
+
+@csrf_exempt
+def snippet_detail(pk,request):
+    try:
+        snippet=Snippet.objects.filter(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method=="GET":
+        pass
+    if request.method=="PUT":
+        pass
+    if request.method=="DELETE":
+        pass
+
+
+
 
